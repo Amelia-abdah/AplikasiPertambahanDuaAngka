a90aba806429cd1744d42f788517db1725c21ce5
Nur Amelia Abdah <r3zk4norhafiz4h@gmail.com>
2025-02-20 20:24:47 +0800

    Initial commit

---
+++ src/latihan1app/Latihan1App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan1app;
+
+/**
+ *
+ * @author User
+ */
+
+import gui.frame;
+public class Latihan1App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frame().setVisible(true);
+    }
+    
+}
